1e0a35a03122e1ea52a5fd9d40dc5189c01a417b
SBanczyk <sbanczyk02@interia.pl>
2024-10-21 17:38:39 +0200

    Edited so the data is merged into 1 dictionary and organised.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+import pprint
 from bs4 import BeautifulSoup
 
 
@@ -11,17 +12,28 @@ def get_otomoto_data(url):
         raise Exception("Tylko samochody osobowe.")
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        keys = soup.find_all("p", {"class": "eizxi837 ooa-y26jp er34gjf0"})
-        values = soup.find_all("p", {"class": "eizxi838 ooa-17xeqrd er34gjf0"})
+        content = BeautifulSoup(response.content, 'html.parser')
+        keys = content.find_all("p", {"class": "e1ho6mkz3 ooa-rlgnr er34gjf0"})
+        values = content.find_all("p", {"class": "e1ho6mkz2 ooa-1rcllto er34gjf0"})
         car_data = {}
+        car_data["Marka"] = content.find_all("h1", {"class": "ooa-1dueukt"})[0].get_text().split(" ")[0]
+        car_data["Portal aukcyjny"] = "OTOMOTO"
         for key, value in zip(keys, values):
-            car_data[key.get_text()] = value.get_text()
-        keys = soup.find_all("p", {"class": "e1ho6mkz3 ooa-rlgnr er34gjf0"})
-        values = soup.find_all("p", {"class": "e1ho6mkz2 ooa-1rcllto er34gjf0"})
+            key = key.get_text().strip()
+            value = value.get_text().strip()
+            if key != "Typ nadwozia":
+                car_data[key] = value
+        keys = content.find_all("p", {"class": "eizxi837 ooa-y26jp er34gjf0"})
+        values = content.find_all("p", {"class": "eizxi838 ooa-17xeqrd er34gjf0"})
         for key, value in zip(keys, values):
-            car_data[key.get_text()] = value.get_text()
-        return car_data
+            key = key.get_text().strip()
+            value = value.get_text().strip()
+            if key in ["Rok produkcji", "Model pojazdu"]:
+                if key == "Model pojazdu":
+                    key = "Model"
+                car_data[key] = value
+
+        return dict(sorted(car_data.items()))
     elif response.status_code == 404:
         raise Exception(f"Status code: {response.status_code}. Błędny link.")
 
@@ -29,23 +41,42 @@ def get_otomoto_data(url):
 def get_olx_data(url):
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
+        content = BeautifulSoup(response.content, 'html.parser')
         categories = []
-        for category in soup.find_all("a", {"class": "css-tyi2d1"}):
+        for category in content.find_all("a", {"class": "css-tyi2d1"}):
             categories.append(category.get_text())
         if "Samochody osobowe" not in categories:
             raise Exception("Tylko samochody osobowe.")
-        keys_values = soup.find_all("p", {"class": "css-b5m1rv"})
+        keys_values = content.find_all("p", {"class": "css-b5m1rv"})
         if len(keys_values) == 0:
             raise Exception("Brak danych. Prawdopodobnie błędny link.")
         car_data = {}
+        car_data["Marka"] = content.find_all("h4", {"class": "css-1kc83jo"})[0].get_text().split(" ")[0]
+        car_data["Portal aukcyjny"] = "OLX"
         for key_value in keys_values:
-            if ":" in key_value.get_text():
-                car_data[key_value.get_text()[0:key_value.get_text().find(":")].strip()] = (
-                    key_value.get_text()[key_value.get_text().find(":")+2:].strip())
-        return car_data
+            key_value = key_value.get_text()
+            if ":" in key_value:
+                key = key_value[0:key_value.find(":")].strip()
+                value = key_value[key_value.find(":")+2:].strip()
+                if key in ["Model", "Rok produkcji", "Przebieg", "Paliwo", "Poj. silnika", "Moc silnika", "Skrzynia biegów"]:
+                    if key == "Moc silnika":
+                        key = "Moc"
+                    if key == "Paliwo":
+                        key = "Rodzaj paliwa"
+                    if key == "Poj. silnika":
+                        key = "Pojemność skokowa"
+                        value = value[:-1] + "3"
+                    car_data[key] = value
+        return dict(sorted(car_data.items()))
 
 
 if __name__ == "__main__":
-    olx = get_olx_data(sys.argv[1])
-    print(olx)
+    comparison_dict = {}
+    for i in [1, 2]:
+        key = f"car_{i}"
+        if "otomoto" in sys.argv[i][0:40]:
+            comparison_dict[key] = get_otomoto_data(sys.argv[i])
+        elif "olx" in sys.argv[i][0:40]:
+            comparison_dict[key] = get_olx_data(sys.argv[i])
+    pprint.pprint(comparison_dict)
+
