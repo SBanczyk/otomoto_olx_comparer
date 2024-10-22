7be57b1b12fc3d09674581d2afd62d2bbaf2a25b
SBanczyk <sbanczyk02@interia.pl>
2024-10-22 13:01:58 +0200

    Added function to change numbers to integer. Cleaned code.

--- get_data.py
+++ get_data.py
@@ -7,31 +7,38 @@ from bs4 import BeautifulSoup
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
 
 
+def clean_value(str_value):
+    if " cm3" in str_value or " km" in str_value or " KM" in str_value or str_value.isnumeric():
+        clean_str = str_value.replace(" km", "").replace(" cm3", "").replace(" KM", "").replace(" ", "")
+        return int(clean_str)
+    return str_value
+
+
 def get_otomoto_data(url):
     if "osobowe" not in url[0:40]:
         raise Exception("Tylko samochody osobowe.")
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        content = BeautifulSoup(response.content, 'html.parser')
-        keys = content.find_all("p", {"class": "e1ho6mkz3 ooa-rlgnr er34gjf0"})
-        values = content.find_all("p", {"class": "e1ho6mkz2 ooa-1rcllto er34gjf0"})
         car_data = {}
-        car_data["Marka"] = content.find_all("h1", {"class": "ooa-1dueukt"})[0].get_text().split(" ")[0]
         car_data["Portal aukcyjny"] = "OTOMOTO"
-        for property, value in zip(keys, values):
+        content = BeautifulSoup(response.content, 'html.parser')
+        car_data["Marka"] = content.find_all("h1", {"class": "ooa-1dueukt"})[0].get_text().split(" ")[0]
+        properties = content.find_all("p", {"class": "e1ho6mkz3 ooa-rlgnr er34gjf0"})
+        values = content.find_all("p", {"class": "e1ho6mkz2 ooa-1rcllto er34gjf0"})
+        for property, value in zip(properties, values):
             property = property.get_text().strip()
             value = value.get_text().strip()
             if property != "Typ nadwozia":
-                car_data[property] = value
-        keys = content.find_all("p", {"class": "eizxi837 ooa-y26jp er34gjf0"})
+                car_data[property] = clean_value(value)
+        properties = content.find_all("p", {"class": "eizxi837 ooa-y26jp er34gjf0"})
         values = content.find_all("p", {"class": "eizxi838 ooa-17xeqrd er34gjf0"})
-        for property, value in zip(keys, values):
+        for property, value in zip(properties, values):
             property = property.get_text().strip()
             value = value.get_text().strip()
             if property in ["Rok produkcji", "Model pojazdu"]:
                 if property == "Model pojazdu":
                     property = "Model"
-                car_data[property] = value
+                car_data[property] = clean_value(value)
 
         return dict(sorted(car_data.items()))
     elif response.status_code == 404:
@@ -42,10 +49,12 @@ def get_olx_data(url):
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         content = BeautifulSoup(response.content, 'html.parser')
-        categories = []
+        is_passenger_car = False
         for category in content.find_all("a", {"class": "css-tyi2d1"}):
-            categories.append(category.get_text())
-        if "Samochody osobowe" not in categories:
+            if category.get_text() == "Samochody osobowe":
+                is_passenger_car = True
+                break
+        if not is_passenger_car:
             raise Exception("Tylko samochody osobowe.")
         properties_values = content.find_all("p", {"class": "css-b5m1rv"})
         if len(properties_values) == 0:
@@ -66,16 +75,24 @@ def get_olx_data(url):
                     if property == "Poj. silnika":
                         property = "Pojemność skokowa"
                         value = value[:-1] + "3"
-                    car_data[property] = value
+                    car_data[property] = clean_value(value)
         return dict(sorted(car_data.items()))
 
 
-if __name__ == "__main__":
+def get_comparison_data(url_1, url_2):
     comparison_dictionary = {}
     for i in [1, 2]:
         car_number = f"car_{i}"
-        if "otomoto" in sys.argv[i][0:40]:
-            comparison_dictionary[car_number] = get_otomoto_data(sys.argv[i])
-        elif "olx" in sys.argv[i][0:40]:
-            comparison_dictionary[car_number] = get_olx_data(sys.argv[i])
-    pprint.pprint(comparison_dictionary)
+        url = url_1 if i == 1 else url_2
+        if "otomoto" in url[0:40]:
+            comparison_dictionary[car_number] = get_otomoto_data(url)
+        elif "olx" in url[0:40]:
+            comparison_dictionary[car_number] = get_olx_data(url)
+    return comparison_dictionary
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        pprint.pprint("Usage: python get_data.py <url_1> <url_2>")
+    else:
+        pprint.pprint(get_comparison_data(sys.argv[1], sys.argv[2]))
