34d747f6298993c3b7c26990af4985b641f5a7b1
SBanczyk <sbanczyk02@interia.pl>
2024-10-22 12:06:25 +0200

    Added proper database creation.

--- create_db.py
+++ create_db.py
@@ -1,12 +1,45 @@
 import sqlite3
 
 
-db_name = "sql.db"
-sqliteConnection = sqlite3.connect(db_name)
-table = """ CREATE TABLE if not exists test (
-            manufacturer char(255),
-            model char(255),
-            production_year char(4)
-            engine_type char(10)
-        ); """
-sqliteConnection.cursor().execute(table)
+def create_db():
+    conn = sqlite3.connect("otomoto_olx_comparer.db")
+    users = """create table if not exists users
+        (
+            user_id integer primary key autoincrement not null,
+            email text not null,
+            password text not null,
+            registration_date text not null default (datetime('now','localtime'))
+        );"""
+    cars = """create table if not exists cars
+        (
+            car_id integer primary key autoincrement not null,
+            brand text not null,
+            model text not null,
+            horse_power int not null,
+            engine_capacity int not null,
+            auction_portal text not null,
+            mileage int not null,
+            fuel_type text not null,
+            production_year text not null,
+            gearbox text not null,
+            url text not null
+        );"""
+    comparisons = """create table if not exists comparisons
+        (
+            comparison_id integer primary key autoincrement not null,
+            user_id integer not null,
+            car1_id integer not null,
+            car2_id integer not null,
+            foreign key(user_id) references users(user_id),
+            foreign key(car1_id) references cars(car_id),
+            foreign key(car2_id) references cars(car_id)
+        );"""
+    conn.cursor().execute(users)
+    conn.commit()
+    conn.cursor().execute(cars)
+    conn.commit()
+    conn.cursor().execute(comparisons)
+    conn.commit()
+
+if __name__ == "__main__":
+    create_db()
