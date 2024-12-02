e5ca7cf8c7955e14a1a2fa7f01e25f8a42ac0d2f
SBanczyk <sbanczyk02@interia.pl>
2024-12-02 10:02:13 +0100

    Cleaned up code.

--- website/views.py
+++ website/views.py
@@ -6,8 +6,4 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    text = """<div class='d-flex justify-content-center align-items-center vh-100' style='max-height: 90vh'>
-                <h3>Prototyp aplikacji do porównywania ogłoszeń z serwisów OTOMOTO i OLX.</h3>
-            </div>
-            """
-    return render_template("home.html", text=text)
+    return render_template("home.html")
