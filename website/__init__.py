afaf38070e77a3c92937c94916e6c3b08bd02d8f
unknown <sbanczyk02@interia.pl>
2025-04-08 17:20:28 +0200

    Restored secret key but to environmental variable.

--- website/__init__.py
+++ website/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from .views import views
 from .auth import auth
@@ -6,6 +7,7 @@ from .comp import comp
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(comp, url_prefix='/')
